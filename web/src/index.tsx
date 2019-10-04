6144565b2b5dacb3b5bd0adb466822d33dcd8e38
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-10-05 06:47:35 +0900

    Use Doc component

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,3 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { Doc } from './Doc';
+
 window.addEventListener('DOMContentLoaded', () => {
   console.log('start');
+  ReactDOM.render(<Doc />, window.document.querySelector('#root'));
 });
