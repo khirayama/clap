70d278c039725162afa2926187d86bb3299648eb
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-10-05 06:47:24 +0900

    Create Doc component

---
+++ web/src/Doc.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface DocProps {
+}
+
+export class Doc extends React.Component {
+  constructor(props: DocProps) {
+    super(props);
+  }
+
+  public render() {
+    return <div>Doc</div>;
+  }
+}
