7ce9bb7b7b49fc6d19a80c288f021f9a1080468c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-10-05 12:45:27 +0900

    Update Doc component

--- web/src/Doc.tsx
+++ web/src/Doc.tsx
@@ -3,9 +3,60 @@ import * as React from 'react';
 interface DocProps {
 }
 
+// https://docs.slatejs.org/guides/data-model
+type Node = Block | Text;
+
+interface Leaf {
+  object: 'leaf';
+  text: string;
+  marks: ('bold' | 'italic' | 'code')[];
+}
+
+interface Document {
+  object: 'document';
+  nodes: Block[];
+}
+
+interface Block {
+  object: 'block';
+  type: 'paragraph';
+  nodes: Node[];
+}
+
+interface Text {
+  object: 'text';
+  leaves: Leaf[];
+}
+
+const sampleDocument: Document = {
+  object: 'document',
+  nodes: [
+    {
+      object: 'block',
+      type: 'paragraph',
+      nodes: [
+        {
+          object: 'text',
+          leaves: [
+            {
+              object: 'leaf',
+              text: '',
+              marks: [],
+            }
+          ],
+        }
+      ],
+    }
+  ],
+};
+
 export class Doc extends React.Component {
   constructor(props: DocProps) {
     super(props);
+
+    this.state = {
+      document: sampleDocument,
+    };
   }
 
   public render() {
