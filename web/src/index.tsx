f443e32394bc4de16fed7f3ed1f42f1b72a1a8d6
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-10-06 13:28:43 +0900

    Use Editor for app

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,9 +1,34 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { Editor } from './clap';
+import * as Clap from './clap';
+
+const sampleDocument: Clap.DocumentProperties = {
+  object: 'document',
+  nodes: [
+    {
+      id: 'vkllllllll',
+      object: 'block',
+      type: 'paragraph',
+      nodes: [
+        {
+          id: 'acda',
+          object: 'text',
+          leaves: [
+            {
+              id: 'cadsca',
+              object: 'leaf',
+              text: '',
+              marks: [],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
 window.addEventListener('DOMContentLoaded', () => {
   console.log('start');
-  ReactDOM.render(<Editor />, window.document.querySelector('#root'));
+  ReactDOM.render(<Clap.Editor document={sampleDocument} />, window.document.querySelector('#root'));
 });
