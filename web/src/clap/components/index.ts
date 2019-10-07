82c52f51a4181c385bb7426eec46272c82de2cf8
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-10-07 22:20:00 +0900

    Update index file for clap components

--- web/src/clap/components/index.ts
+++ web/src/clap/components/index.ts
@@ -1,1 +1,8 @@
+import { ComponentPool } from './ComponentPool';
+import { Paragraph } from './Paragraph';
+
 export { Editor } from './Editor';
+export { ComponentPool } from './ComponentPool';
+export { Paragraph } from './Paragraph';
+
+ComponentPool.register('paragraph', Paragraph);
