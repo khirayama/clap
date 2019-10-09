28dbfae3d1e6d17c11ab857024876b4e9193f9e2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-10-09 21:27:11 +0900

    Update clap components

--- web/src/clap/components/Editor.tsx
+++ web/src/clap/components/Editor.tsx
@@ -3,7 +3,16 @@ import styled from 'styled-components';
 
 import * as ClapNode from '../nodes/index';
 import { ComponentPool } from './ComponentPool';
-import { Item } from './Item';
+import { Item, ItemProps } from './Item';
+
+const KEY_CODE = {
+  ENTER: 13,
+  ESC: 27,
+  UP: 38,
+  DOWN: 40,
+  J: 74,
+  K: 75,
+};
 
 const Wrapper = styled.div`
   font-family: sans-serif;
@@ -32,8 +41,6 @@ interface EditorState {
 }
 
 export class Editor extends React.Component<EditorProps, EditorState> {
-  private document: ClapNode.DocumentNode;
-
   constructor(props: EditorProps) {
     super(props);
 
@@ -46,15 +53,125 @@ export class Editor extends React.Component<EditorProps, EditorState> {
       },
       document: this.props.document.toJSON(),
     };
+
+    this.onClickItem = this.onClickItem.bind(this);
+    this.onFocusItem = this.onFocusItem.bind(this);
+    this.onKeyDownItem = this.onKeyDownItem.bind(this);
+    this.onKeyPressItem = this.onKeyPressItem.bind(this);
+    this.onKeyUpItem = this.onKeyUpItem.bind(this);
+  }
+
+  private onClickItem(event: React.MouseEvent<HTMLDivElement>, itemProps: ItemProps) {
+    this.setState({
+      cursor: {
+        id: itemProps.node.id,
+        mode: 'insert',
+      },
+    });
+  }
+
+  private onFocusItem(event: React.FormEvent<HTMLDivElement>, itemProps: ItemProps) {
+    this.setState({
+      cursor: {
+        id: itemProps.node.id,
+        mode: 'select',
+      },
+    });
+  }
+
+  private onKeyDownItem(event: React.KeyboardEvent<HTMLDivElement>, itemProps: ItemProps) {
+    const keyCode = event.keyCode;
+    const meta = event.metaKey;
+    const shift = event.shiftKey;
+    const cursor = this.state.cursor;
+    const currentNode = this.props.document.find(cursor.id);
+
+    if (
+      // mode === 'select' &&
+      keyCode === KEY_CODE.DOWN
+      // meta
+      // shift
+    ) {
+      let targetNode = null;
+      if (currentNode.nodes[0]) {
+        targetNode = currentNode.nodes[0];
+      } else if (currentNode.next()) {
+        targetNode = currentNode.next();
+      } else {
+        targetNode = currentNode;
+        while (targetNode && !targetNode.next()) {
+          targetNode = targetNode.parent();
+        }
+        if (targetNode) {
+          targetNode = targetNode.next();
+        }
+      }
+      if (targetNode) {
+        this.setState({
+          cursor: {
+            id: targetNode.id,
+            mode: cursor.mode,
+          },
+        });
+      }
+    } else if (
+      // mode === 'select' &&
+      keyCode === KEY_CODE.UP
+      // meta
+      // shift
+    ) {
+      let targetNode = null;
+      if (currentNode.prev()) {
+        targetNode = currentNode.prev();
+        while (targetNode && targetNode.nodes.length) {
+          targetNode = targetNode.nodes[targetNode.nodes.length - 1];
+        }
+      } else {
+        if (currentNode.parent().object !== 'document') {
+          targetNode = currentNode.parent();
+        }
+      }
+      if (targetNode) {
+        this.setState({
+          cursor: {
+            id: targetNode.id,
+            mode: cursor.mode,
+          },
+        });
+      }
+    }
+    // console.log(keyCode, meta, shift);
+    // console.log(event, itemProps);
+  }
+
+  private onKeyPressItem(event: React.KeyboardEvent<HTMLDivElement>, itemProps: ItemProps) {
+    const keyCode = event.keyCode;
+    // console.log(keyCode);
+    // console.log(event, itemProps);
+  }
+
+  private onKeyUpItem(event: React.KeyboardEvent<HTMLDivElement>, itemProps: ItemProps) {
+    const keyCode = event.keyCode;
+    // console.log(keyCode);
+    // console.log(event, itemProps);
   }
 
   private renderLines(nodes: ClapNode.PureItemNode[], indent: number = 0, lines: JSX.Element[] = []): JSX.Element[] {
     for (const node of nodes) {
       const Component = ComponentPool.take(node.type);
       if (Component) {
-        const mode = node.id === this.state.cursor.id ? 'select' : 'normal';
         lines.push(
-          <Item key={node.id} indent={indent} cursorMode={mode}>
+          <Item
+            key={node.id}
+            indent={indent}
+            node={node}
+            cursor={this.state.cursor}
+            onClick={this.onClickItem}
+            onFocus={this.onFocusItem}
+            onKeyDown={this.onKeyDownItem}
+            onKeyPress={this.onKeyPressItem}
+            onKeyUp={this.onKeyUpItem}
+          >
             <Component node={node} />
           </Item>,
         );
--- web/src/clap/components/Item.tsx
+++ web/src/clap/components/Item.tsx
@@ -2,18 +2,18 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 import { Cursor } from './Editor';
+import * as ClapNode from '../nodes/index';
 
-interface ItemProps {
+export interface ItemProps {
   indent: number;
-  cursorMode: Cursor['mode'];
+  cursor: Cursor;
+  node: ClapNode.PureItemNode;
+  onClick: (event: React.MouseEvent<HTMLDivElement>, props: ItemProps) => void;
+  onFocus: (event: React.FormEvent<HTMLDivElement>, props: ItemProps) => void;
+  onKeyDown: (event: React.KeyboardEvent<HTMLDivElement>, props: ItemProps) => void;
+  onKeyPress: (event: React.KeyboardEvent<HTMLDivElement>, props: ItemProps) => void;
+  onKeyUp: (event: React.KeyboardEvent<HTMLDivElement>, props: ItemProps) => void;
 }
-
-const KEY_CODE = {
-  ENTER: 13,
-  ESC: 27,
-  UP: 38,
-  DOWN: 40,
-};
 
 const Wrapper = styled.div`
   box-sizing: border-box;
@@ -25,44 +25,50 @@ const Wrapper = styled.div`
   -webkit-appearance: none;
 
   padding: 0 0 0 ${(props: ItemProps) => `${props.indent * 10}px`};
-  background: ${(props: ItemProps) => (props.cursorMode === 'select' ? 'rgba(45, 170, 219, 0.3)' : 'transparent')};
-
-  &:focus {
-    background: red;
-  }
+  background: ${(props: ItemProps) => {
+    if (props.cursor.id === props.node.id && props.cursor.mode === 'select') {
+      return 'rgba(45, 170, 219, 0.3)';
+    } else if (props.cursor.id === props.node.id && props.cursor.mode === 'insert') {
+      return 'red';
+    }
+    return 'transparent';
+  }};
 `;
 
 export class Item extends React.Component<ItemProps> {
   public render(): JSX.Element {
+    const onClick = (event: React.MouseEvent<HTMLDivElement>) => {
+      event.stopPropagation();
+      this.props.onClick(event, this.props);
+    };
+
     const onFocus = (event: React.FormEvent<HTMLDivElement>) => {
-      console.log(event);
-      console.log('focus');
+      event.stopPropagation();
+      this.props.onFocus(event, this.props);
     };
 
     const onKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
-      const keyCode = event.keyCode;
-      console.log(event.type, keyCode);
-      if (keyCode === KEY_CODE.ENTER) {
-        event.preventDefault();
-      }
+      event.stopPropagation();
+      this.props.onKeyDown(event, this.props);
     };
 
     const onKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
-      const keyCode = event.keyCode;
-      console.log(event.type, keyCode);
+      event.stopPropagation();
+      this.props.onKeyPress(event, this.props);
     };
 
     const onKeyUp = (event: React.KeyboardEvent<HTMLDivElement>) => {
-      const keyCode = event.keyCode;
-      console.log(event.type, keyCode);
+      event.stopPropagation();
+      this.props.onKeyUp(event, this.props);
     };
 
     return (
       <Wrapper
         tabIndex={0}
         indent={this.props.indent}
-        cursorMode={this.props.cursorMode}
-        contentEditable={this.props.cursorMode === 'insert'}
+        cursor={this.props.cursor}
+        node={this.props.node}
+        onClick={onClick}
         onFocus={onFocus}
         onKeyDown={onKeyDown}
         onKeyPress={onKeyPress}
