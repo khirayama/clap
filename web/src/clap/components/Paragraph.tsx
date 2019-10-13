66abdc1c71b30ffaed168a2cf26d3da73183f12a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-10-13 13:05:20 +0900

    Use components for paragraph

--- web/src/clap/components/Paragraph.tsx
+++ web/src/clap/components/Paragraph.tsx
@@ -26,12 +26,89 @@ export class Paragraph extends React.Component<ParagraphProps> {
 
   public ref: React.RefObject<HTMLParagraphElement> = React.createRef();
 
-  public render(): JSX.Element {
-    const node = this.props.node;
+  public html(): string {
+    return this.props.node.attributes.leaves
+      .map(leaf => {
+        let style = '';
+        let tag = 'span';
+        let attributes = [];
+        for (const mark of leaf.marks) {
+          switch (mark.type) {
+            case 'bold': {
+              style += 'font-weight: bold;';
+              break;
+            }
+            case 'italic': {
+              style += 'font-style: italic;';
+              break;
+            }
+            case 'code': {
+              tag = 'code';
+              break;
+            }
+            case 'strike': {
+              style += 'text-decoration: line-through;';
+              break;
+            }
+            case 'link': {
+              tag = 'a';
+              attributes.push(`href="${mark.href}" onclick="window.alert('hey')"`);
+              break;
+            }
+          }
+        }
+        return `<${tag} style="${style}" ${attributes.join(' ')}>${leaf.text}</${tag}>`;
+      })
+      .join('');
+  }
 
+  public renderLeaves(): JSX.Element[] {
+    return this.props.node.attributes.leaves.map(leaf => {
+      let tag = 'span';
+      let style: any = {};
+      let attributes: any = {};
+      for (const mark of leaf.marks) {
+        switch (mark.type) {
+          case 'bold': {
+            style.fontWeight = 'bold';
+            break;
+          }
+          case 'italic': {
+            style.fontStyle = 'italic';
+            break;
+          }
+          case 'code': {
+            tag = 'code';
+            break;
+          }
+          case 'strike': {
+            style.textDecoration = 'line-through';
+            break;
+          }
+          case 'link': {
+            tag = 'a';
+            attributes.href = mark.href;
+            break;
+          }
+        }
+      }
+      return React.createElement(
+        tag,
+        {
+          key: leaf.id,
+          ...attributes,
+          style,
+        },
+        leaf.text,
+      );
+    });
+  }
+
+  // dangerouslySetInnerHTML={{ __html: this.html() }}
+  public render(): JSX.Element {
     return (
       <Wrapper ref={this.ref} contentEditable suppressContentEditableWarning={true}>
-        {node.attributes.text}
+        {this.renderLeaves()}
       </Wrapper>
     );
   }
