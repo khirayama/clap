9a7e67390628d09efc8630dc6a5c180b3797537b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-10-15 08:07:43 +0900

    Add export

--- web/src/clap/components/index.ts
+++ web/src/clap/components/index.ts
@@ -3,6 +3,8 @@ import { Paragraph } from './Paragraph';
 
 export { Editor } from './Editor';
 export { ComponentPool } from './ComponentPool';
+export { Item } from './Item';
+export { KeyBinder } from './KeyBinder';
 export { Paragraph } from './Paragraph';
 
 ComponentPool.register('paragraph', Paragraph);
