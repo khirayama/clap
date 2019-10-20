c286a489b07cb1c116d69551b3f031a70559503b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-10-20 15:28:36 +0900

    Export USECASE

--- web/src/clap/index.ts
+++ web/src/clap/index.ts
@@ -1,6 +1,6 @@
 export { ComponentPool, Editor, Item, ItemText } from './components';
 export { PureSelection, Selection } from './Selection';
-export { Emitter, EmitterPayload, createEmitter } from './emitter';
+export { Emitter, EmitterPayload, createEmitter, USECASE } from './emitter';
 export {
   ItemNodePool,
   PureNode,
