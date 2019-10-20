16ed59d13fb3127fe60f53822c4bd4321ba99eb2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-10-20 15:07:54 +0900

    Create createEmitter

---
+++ web/src/clap/createEmitter.ts
@@ -0,0 +1,12 @@
+import * as Clap from './index';
+
+export interface EmitterPayload {
+  document: Clap.DocumentNode;
+  selection: Clap.Selection;
+}
+
+export function createEmitter() {
+  const emitter = new Clap.Emitter<EmitterPayload>();
+
+  return emitter;
+}
