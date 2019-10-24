e77052004070a06999249d5b0b13407f141dfd5b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-10-24 17:35:03 +0900

    Remove needless focus function

--- web/src/clap/components/utils.ts
+++ web/src/clap/components/utils.ts
@@ -1,30 +1,5 @@
 import * as Clap from '../index';
 
-export function focus(el: HTMLElement, position: 'beginning' | 'end' = 'end'): void {
-  // FYI: https://stackoverflow.com/questions/4233265/contenteditable-set-caret-at-the-end-of-the-text-cross-browser
-  const range: Range = document.createRange();
-  /*
-    FYI: If you use following lines, it doesn't work. It returns selection start and end are 1.
-    Maybe it is react's problem. Raw content editable doesn't have.
-    ```
-    range.selectNodeContents(el);
-    range.collapse(false);
-    ```
-   */
-  let targetNode = el.childNodes[el.childNodes.length - 1] || el;
-  let pos = targetNode.textContent.length;
-  if (position === 'beginning') {
-    targetNode = el.childNodes[0] || el;
-    pos = 0;
-  }
-  range.setStart(targetNode.childNodes[0] || targetNode, pos);
-  range.setEnd(targetNode.childNodes[0] || targetNode, pos);
-
-  const selection: Selection = window.getSelection();
-  selection.removeAllRanges();
-  selection.addRange(range);
-}
-
 export function findUpperNode(currentNode: Clap.Node): Clap.Node | null {
   let targetNode = null;
   if (currentNode.prev()) {
