cc90d9f8e82bf2825158c16361f4d8334f7757e2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-10-25 07:12:37 +0900

    Set default text content in paragraph node

--- web/src/clap/nodes/item/ParagraphNode.ts
+++ web/src/clap/nodes/item/ParagraphNode.ts
@@ -1,11 +1,13 @@
 import { PureNode, BaseNode } from '../BaseNode';
 import { BaseItemNode } from './BaseItemNode';
+import { TextContent } from '../content/TextContent';
 
 export class ParagraphNode extends BaseItemNode {
   constructor(node?: Partial<PureNode>, relations?: BaseNode['relations']) {
     super(node, relations);
 
     this.type = 'paragraph';
+    this.contents.push(new TextContent());
   }
 
   public isParagraphNode(): this is ParagraphNode {
