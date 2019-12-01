6efeabcd4c87b607dea0674a4ded270270ad0624
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-12-01 14:23:04 +0900

    Update Editor

--- redesign/src/clap/components/Editor.tsx
+++ redesign/src/clap/components/Editor.tsx
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 import { DocumentNode } from '../nodes';
 import { Selection } from '../selection';
 
+import * as Clap from '../index';
+
 const Wrapper = styled.div``;
 
 interface EditorProps {
@@ -12,14 +14,36 @@ interface EditorProps {
 }
 
 export function Editor(props: EditorProps) {
+  const [document, setDocument] = React.useState(props.document.toJSON());
+  const [selection, setSelection] = React.useState(props.selection.toJSON());
+
   React.useEffect(() => {
     console.log('mount');
     props.document.on(() => {
-      console.log('change');
+      console.log('change document');
+      setDocument(props.document.toJSON());
     });
     props.selection.on(() => {
-      console.log('change');
+      console.log('change selection');
+      setSelection(props.selection.toJSON());
     });
   }, []);
-  return <Wrapper />;
+
+  function renderItems(nodes: Clap.PureNode[], indent: number = 0, items: JSX.Element[] = []) {
+    for (const node of nodes) {
+      items.push(
+        <Clap.Item key={node.id} indent={indent} selection={selection} node={node}>
+          Item
+        </Clap.Item>,
+      );
+
+      if (node.nodes) {
+        items.concat(renderItems(node.nodes, indent + 1, items));
+      }
+    }
+
+    return items;
+  }
+
+  return <Wrapper>{renderItems(document.nodes)}</Wrapper>;
 }
