be3cfd9dfeb7890ec228003e7c357c0fa1e81114
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-12-10 07:46:21 +0900

    Update selection rendering

--- redesign/src/clap/components/Inline.tsx
+++ redesign/src/clap/components/Inline.tsx
@@ -4,11 +4,44 @@ import styled from 'styled-components';
 import * as Clap from '../index';
 
 export interface InlineProps {
-  content: Clap.PureContent;
+  selection: Clap.PureSelection;
+  contents: Clap.PureContent[];
 }
 
-const Wrapper = styled.span``;
+const Wrapper = styled.span`
+  ${(props: { isInRange: boolean; isClosed: boolean }) => (props.isInRange ? 'background: red' : '')};
+  ${(props: { isInRange: boolean; isClosed: boolean }) => (props.isClosed ? 'border-right: solid 1px #aaa' : '')};
+`;
 
 export function Inline(props: InlineProps) {
-  return <Wrapper>{props.content.text}</Wrapper>;
+  const contents = props.contents;
+  const anchor = props.selection.range.anchor;
+  const focus = props.selection.range.focus;
+  let isClosed = false;
+  let isStarted = false;
+  // TODO: selectionを分割するのがよい。aとかを分割したくないから
+
+  return (
+    <>
+      {contents.map(content => {
+        // TODO: Check Japanese Chara
+        return content.text.split('').map((chara, i) => {
+          isClosed = false;
+          if (anchor.id === content.id && focus.id === content.id && anchor.offset === i && focus.offset === i) {
+            isClosed = true;
+          } else if (
+            (anchor.id === content.id && anchor.offset === i) ||
+            (focus.id === content.id && focus.offset === i)
+          ) {
+            isStarted = !isStarted;
+          }
+          return (
+            <Wrapper key={`${content.id}-${i}`} isInRange={isStarted} isClosed={isClosed}>
+              {chara}
+            </Wrapper>
+          );
+        });
+      })}
+    </>
+  );
 }
