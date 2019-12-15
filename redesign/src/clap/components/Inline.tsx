7337dc9f4ccac92bab0c213d313abe4ad148491b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-12-15 22:46:57 +0900

    Update Inline

--- redesign/src/clap/components/Inline.tsx
+++ redesign/src/clap/components/Inline.tsx
@@ -9,15 +9,14 @@ export interface InlineProps {
 }
 
 const Wrapper = styled.span`
-  ${(props: { isInRange: boolean; isClosed: boolean }) => (props.isInRange ? 'background: red' : '')};
-  ${(props: { isInRange: boolean; isClosed: boolean }) => (props.isClosed ? 'border-right: solid 1px #aaa' : '')};
+  ${(props: { isInRange: boolean }) => (props.isInRange ? 'background: red' : '')};
 `;
 
 export function Inline(props: InlineProps) {
   const contents = props.contents;
   const anchor = props.selection.range.anchor;
   const focus = props.selection.range.focus;
-  let isClosed = false;
+  let isClosed = anchor.id === focus.id && anchor.offset === focus.offset;
   let isStarted = false;
   // TODO: selectionを分割するのがよい。aとかを分割したくないから
 
@@ -26,17 +25,15 @@ export function Inline(props: InlineProps) {
       {contents.map(content => {
         // TODO: Check Japanese Chara
         return content.text.split('').map((chara, i) => {
-          isClosed = false;
-          if (anchor.id === content.id && focus.id === content.id && anchor.offset === i && focus.offset === i) {
-            isClosed = true;
-          } else if (
-            (anchor.id === content.id && anchor.offset === i) ||
-            (focus.id === content.id && focus.offset === i)
+          if (
+            !isClosed &&
+            ((anchor.id === content.id && anchor.offset === i) || (focus.id === content.id && focus.offset === i))
           ) {
             isStarted = !isStarted;
           }
           return (
-            <Wrapper key={`${content.id}-${i}`} isInRange={isStarted} isClosed={isClosed}>
+            <Wrapper key={`${content.id}-${i}`} isInRange={isStarted}>
+              {isClosed && anchor.id === content.id && anchor.offset === i ? '|' : null}
               {chara}
             </Wrapper>
           );
