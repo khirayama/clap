e167807b2c18316759d406201390e8028f5dca0f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-04-19 08:58:03 +0900

    Create nodeMap

---
+++ 20200417/src/nodes/nodeMap.ts
@@ -0,0 +1,7 @@
+import { DocumentNode, ParagraphNode, HorizontalRuleNode } from './index';
+
+export const nodeMap = {
+  document: DocumentNode,
+  paragraph: ParagraphNode,
+  horizontalrule: HorizontalRuleNode,
+};
