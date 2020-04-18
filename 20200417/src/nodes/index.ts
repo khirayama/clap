183b875ad05aac06bbec0485bc0058b7dcf5e756
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-04-19 08:58:17 +0900

    Create index file for nodes

---
+++ 20200417/src/nodes/index.ts
@@ -0,0 +1,9 @@
+import { ParagraphNode } from './ParagraphNode';
+
+export { nodeMap } from './nodeMap';
+export { DocumentNode } from './DocumentNode';
+export { ParagraphNode } from './ParagraphNode';
+export { HorizontalRuleNode } from './HorizontalRuleNode';
+export { Node, NodeType } from './Node';
+
+export type ItemNode = ParagraphNode;
