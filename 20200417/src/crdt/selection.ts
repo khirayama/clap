cdbe435d6b04df5a4c06f0a2e0d4c90e151b6827
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-04-24 07:49:53 +0900

    Create selection for crdt

---
+++ 20200417/src/crdt/selection.ts
@@ -0,0 +1,38 @@
+import * as Automerge from 'automerge';
+
+export const utils = {
+  mode: (selection: Selection): 'normal' | 'select' | 'insert' => {
+    if (selection.ids.length === 0) {
+      return 'normal';
+    } else if (selection.range === null) {
+      return 'select';
+    } else {
+      return 'insert';
+    }
+  },
+
+  isCollasped: (selection: Selection): boolean => {
+    return !!(
+      selection.ids.length === 1 &&
+      selection.range &&
+      selection.range.anchor.id === selection.range.focus.id &&
+      selection.range.anchor.offset.value === selection.range.focus.offset.value
+    );
+  },
+};
+
+export type Selection = {
+  isComposing: boolean;
+  compositionText: string;
+  ids: string[];
+  range: {
+    anchor: {
+      id: string;
+      offset: Automerge.Counter;
+    };
+    focus: {
+      id: string;
+      offset: Automerge.Counter;
+    };
+  } | null;
+};
