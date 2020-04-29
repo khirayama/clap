214cfa68acadfb9507fbe74f58303a2b9007cc5c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-04-29 11:50:23 +0900

    Update example spec

--- CRDT/src/example.spec.ts
+++ CRDT/src/example.spec.ts
@@ -1,12 +1,1 @@
-import * as assert from 'power-assert';
-
 import './example';
-
-describe('Run', () => {
-  it('Correctly', () => {
-    console.log('run by mocha');
-  });
-  it('Throw Error', () => {
-    throw new Error('tada!');
-  });
-});
