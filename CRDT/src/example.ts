ec79d4065d3a24099fdb46628efcf1af8e9f2dd5
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-04-29 22:18:18 +0900

    Use usecase

--- CRDT/src/example.ts
+++ CRDT/src/example.ts
@@ -1,13 +1,17 @@
 import * as Automerge from 'automerge';
 
+import { CRDTDocument } from './CRDTDocument';
+import { usecase } from './usecase';
 import { factory } from './factory';
 import { transform } from './transform';
-import { CRDTDocument } from './CRDTDocument';
 
-type Doc = ReturnType<typeof factory.utils.init>;
+type Doc = ReturnType<typeof usecase.init>;
 
 // Login
 const user = {
+  id: Automerge.uuid(),
+};
+const member = {
   id: Automerge.uuid(),
 };
 
@@ -15,17 +19,13 @@ const user = {
 let userDoc = new CRDTDocument(user.id);
 
 // [member] Join member
-const member = {
-  id: Automerge.uuid(),
-};
-
 let memberDoc = new CRDTDocument(member.id, userDoc.save());
 memberDoc.change((doc: Doc) => {
   const selection = factory.selection.createSelection();
   doc.users[member.id] = selection;
 });
 
-// [user][members] Merge
+// [user][members] Sync from member
 userDoc.merge(new CRDTDocument(member.id, memberDoc.save()));
 memberDoc.merge(new CRDTDocument(user.id, userDoc.save()));
 
@@ -54,7 +54,7 @@ memberDoc.merge(new CRDTDocument(user.id, userDoc.save()));
 
 // [user] Insert text by user
 userDoc.change((userDoc) => {
-  transform.util.insertText(user.id, userDoc.users, userDoc.document, ['H', 'e', 'l', 'l', 'o', ' ']);
+  usecase.insertText(user.id, userDoc.users, userDoc.document, ['H', 'e', 'l', 'l', 'o', ' ']);
 });
 
 // [user][members] Merge
