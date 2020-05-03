2a435cfbb88c5318f9d806b1396bd0f303b49205
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-05-03 13:50:17 +0900

    Implement deleteText

--- CRDT/src/actions.ts
+++ CRDT/src/actions.ts
@@ -73,17 +73,45 @@ export const actions = {
     }
   },
 
-  deleteText: (userId: string, doc: Doc, chars: string[]): void => {
-    let TODO = false;
+  deleteText: (userId: string, doc: Doc): void => {
+    const users = doc.users;
+    const document = doc.document;
+    const selection: Selection = users[userId];
 
-    if (/* TODO: 編集者選択範囲始点がノードのインライン先頭の先頭だった場合 */ TODO) {
-      // noop
-    } else {
+    if (selection.range === null || !selectionUtils.isCollasped(selection)) return;
+
+    const node = traversal.node.findCurrentNode(selection, document);
+
+    if (node === null || node.inline === null) return;
+
+    const inlineId = selection.range.anchor.id;
+    const inline = traversal.inline.find(node, inlineId);
+
+    if (inline === null) return;
+
+    const offset = selection.range.anchor.offset.value;
+    transform.inline.delete(inline, offset - 1, 1);
+
+    const userIds = Object.keys(users);
+    if (selection.range.anchor.id === inlineId && selection.range.anchor.offset.value >= offset) {
+      selection.range.anchor.offset.decrement(1);
     }
-
-    // post process
-    if (/* TODO: インラインのテキストが空になった場合*/ TODO) {
-      // noop
+    if (selection.range.focus.id === selection.range.focus.id && selection.range.focus.offset.value >= offset) {
+      selection.range.focus.offset.decrement(1);
+    }
+    for (const uid of userIds) {
+      const slctn = users[uid];
+      if (slctn && slctn.range && slctn.ids[0] === node.id) {
+        if (uid !== userId) {
+          // 共同編集者への処理
+          if (slctn.range.anchor.id === inlineId && slctn.range.anchor.offset.value > offset) {
+            slctn.range.anchor.offset.decrement(1);
+          }
+          if (slctn.range.focus.id === inlineId && slctn.range.focus.offset.value > offset) {
+            slctn.range.focus.offset.decrement(1);
+          }
+        }
+      }
     }
   },
 };
