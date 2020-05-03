227ba54598e7495c82436eeb64bc70820eef5265
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-05-03 15:12:01 +0900

    Add test cases

--- CRDT/src/actions.spec.ts
+++ CRDT/src/actions.spec.ts
@@ -105,6 +105,41 @@ describe('.insertText()', () => {
 
         assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
       });
+
+      it('文字列が挿入されて共同編集者の選択範囲の始点と終点が逆位置の場合でも始点が文字数分後ろへ移動していること', () => {
+        const expectedDoc = toLooseJSON(userDoc);
+        const inlineText = expectedDoc.doc.document.nodes[0].inline[0];
+        const userSelection = expectedDoc.doc.users[user.id];
+        const memberSelection = expectedDoc.doc.users[member.id];
+        inlineText.text = ['1', 'あ', 'い', 'う', 'え', 'お', '2', '3'];
+        userSelection.range.anchor.offset = 6;
+        userSelection.range.focus.offset = 6;
+        memberSelection.range.anchor.offset = 7;
+        memberSelection.range.focus.offset = 0;
+
+        userDoc.change((doc) => {
+          actions.insertText(user.id, doc, ['1', '2', '3']);
+        });
+        memberDoc.merge(userDoc);
+        memberDoc.change((doc) => {
+          const range = doc.users[member.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(selectionUtils.getOffset(range.anchor.offset.value, 2));
+            range.focus.offset.increment(selectionUtils.getOffset(range.focus.offset.value, 0));
+          }
+        });
+        userDoc.merge(memberDoc);
+        userDoc.change((doc) => {
+          const range = doc.users[user.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(selectionUtils.getOffset(range.anchor.offset.value, 1));
+            range.focus.offset.increment(selectionUtils.getOffset(range.focus.offset.value, 1));
+          }
+          actions.insertText(user.id, doc, ['あ', 'い', 'う', 'え', 'お']);
+        });
+
+        assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
+      });
     });
   });
 });
@@ -200,6 +235,46 @@ describe('.deleteText()', () => {
 
         assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
       });
+
+      it('選択範囲始点の前の文字が1文字削除されて、共同編集者選択範囲の始点と終点が逆位置でも始点が1文字前に移動していること', () => {
+        const expectedDoc = toLooseJSON(userDoc);
+        const inlineText = expectedDoc.doc.document.nodes[0].inline[0];
+        const userSelection = expectedDoc.doc.users[user.id];
+        const memberSelection = expectedDoc.doc.users[member.id];
+        inlineText.text = ['あ', 'う', 'え', 'お'];
+        userSelection.range.anchor.offset = 1;
+        userSelection.range.focus.offset = 1;
+        memberSelection.range.anchor.offset = 3;
+        memberSelection.range.focus.offset = 0;
+
+        userDoc.change((doc) => {
+          actions.insertText(user.id, doc, ['あ', 'い', 'う', 'え', 'お']);
+        });
+        memberDoc.merge(userDoc);
+        memberDoc.change((doc) => {
+          const range = doc.users[member.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(selectionUtils.getOffset(range.anchor.offset.value, 4));
+            range.focus.offset.increment(selectionUtils.getOffset(range.focus.offset.value, 0));
+          }
+        });
+        userDoc.merge(memberDoc);
+        userDoc.change((doc) => {
+          const range = doc.users[user.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(selectionUtils.getOffset(range.anchor.offset.value, 2));
+            range.focus.offset.increment(selectionUtils.getOffset(range.focus.offset.value, 2));
+          }
+          actions.deleteText(user.id, doc);
+        });
+
+        assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
+      });
+    });
+  });
+  describe('選択範囲が開いている状態で', () => {
+    describe('文字削除を行ったとき', () => {
+      it('選択範囲の文字が削除されて、選択範囲始点と終点が文字数分前に移動していること', () => {});
     });
   });
 });
