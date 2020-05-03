2778ebc0cc6ba2d481011df15666d540701d01d6
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-05-03 13:49:47 +0900

    Add test cases

--- CRDT/src/actions.spec.ts
+++ CRDT/src/actions.spec.ts
@@ -108,3 +108,98 @@ describe('.insertText()', () => {
     });
   });
 });
+
+describe('.deleteText()', () => {
+  describe('選択範囲が閉じている状態で', () => {
+    describe('文字削除を行ったとき', () => {
+      it('選択範囲始点の前の文字が1文字削除されて、選択範囲始点と終点が1文字前に移動していること', () => {
+        userDoc.change((doc) => {
+          actions.insertText(user.id, doc, ['あ', 'い', 'う', 'え', 'お']);
+        });
+
+        const expectedDoc = toLooseJSON(userDoc);
+        const inlineText = expectedDoc.doc.document.nodes[0].inline[0];
+        const userSelection = expectedDoc.doc.users[user.id];
+        inlineText.text = ['あ', 'い', 'う', 'え'];
+        userSelection.range.anchor.offset = 4;
+        userSelection.range.focus.offset = 4;
+
+        userDoc.change((doc) => {
+          actions.deleteText(user.id, doc);
+        });
+
+        assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
+      });
+
+      it('選択範囲始点の前の文字が1文字削除されて、共同編集者選択範囲始点が1文字前に移動していること', () => {
+        const expectedDoc = toLooseJSON(userDoc);
+        const inlineText = expectedDoc.doc.document.nodes[0].inline[0];
+        const userSelection = expectedDoc.doc.users[user.id];
+        const memberSelection = expectedDoc.doc.users[member.id];
+        inlineText.text = ['あ', 'う', 'え', 'お'];
+        userSelection.range.anchor.offset = 1;
+        userSelection.range.focus.offset = 1;
+        memberSelection.range.anchor.offset = 4;
+        memberSelection.range.focus.offset = 4;
+
+        userDoc.change((doc) => {
+          actions.insertText(user.id, doc, ['あ', 'い', 'う', 'え', 'お']);
+        });
+        memberDoc.merge(userDoc);
+        memberDoc.change((doc) => {
+          const range = doc.users[member.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(selectionUtils.getOffset(range.anchor.offset.value, 5));
+            range.focus.offset.increment(selectionUtils.getOffset(range.focus.offset.value, 5));
+          }
+        });
+        userDoc.merge(memberDoc);
+        userDoc.change((doc) => {
+          const range = doc.users[user.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(selectionUtils.getOffset(range.anchor.offset.value, 2));
+            range.focus.offset.increment(selectionUtils.getOffset(range.focus.offset.value, 2));
+          }
+          actions.deleteText(user.id, doc);
+        });
+
+        assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
+      });
+
+      it('文字列が挿入されて共同編集者の選択範囲終点が文字数分後ろへ移動していること', () => {
+        const expectedDoc = toLooseJSON(userDoc);
+        const inlineText = expectedDoc.doc.document.nodes[0].inline[0];
+        const userSelection = expectedDoc.doc.users[user.id];
+        const memberSelection = expectedDoc.doc.users[member.id];
+        inlineText.text = ['あ', 'う', 'え', 'お'];
+        userSelection.range.anchor.offset = 1;
+        userSelection.range.focus.offset = 1;
+        memberSelection.range.anchor.offset = 0;
+        memberSelection.range.focus.offset = 3;
+
+        userDoc.change((doc) => {
+          actions.insertText(user.id, doc, ['あ', 'い', 'う', 'え', 'お']);
+        });
+        memberDoc.merge(userDoc);
+        memberDoc.change((doc) => {
+          const range = doc.users[member.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(selectionUtils.getOffset(range.anchor.offset.value, 0));
+            range.focus.offset.increment(selectionUtils.getOffset(range.focus.offset.value, 4));
+          }
+        });
+        userDoc.merge(memberDoc);
+        userDoc.change((doc) => {
+          const range = doc.users[user.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(selectionUtils.getOffset(range.anchor.offset.value, 2));
+            range.focus.offset.increment(selectionUtils.getOffset(range.focus.offset.value, 2));
+          }
+          actions.deleteText(user.id, doc);
+        });
+
+        assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
+      });
+    });
+  });
+});
