1a7b039092733956f4ac6e6f26974a60447c7223
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-05-04 16:16:24 +0900

    Make actions primitive

--- CRDT/src/actions.ts
+++ CRDT/src/actions.ts
@@ -86,44 +86,80 @@ export const actions = {
     const document = doc.document;
     const selection: Selection = users[userId];
 
-    if (selection.range !== null && sutils.isCollasped(selection)) {
-      const node = traversal.node.findCurrentNode(selection, document);
+    if (selection.range === null) return;
 
-      if (node === null || node.inline === null) return;
+    const node = traversal.node.findCurrentNode(selection, document);
 
-      const inlineId = selection.range.anchor.id;
-      const inline = traversal.inline.find(node, inlineId);
+    if (node === null || node.inline === null) return;
 
-      if (inline === null) return;
+    const inlineId = selection.range.anchor.id;
+    const inline = traversal.inline.find(node, inlineId);
 
-      const offset = selection.range.anchor.offset.value;
-      transform.inline.insertText(inline, offset, chars);
+    if (inline === null) return;
 
-      const userIds = Object.keys(users);
-      if (selection.range.anchor.id === inlineId && selection.range.anchor.offset.value >= offset) {
-        selection.range.anchor.offset.increment(chars.length);
-      }
-      if (selection.range.focus.id === selection.range.focus.id && selection.range.focus.offset.value >= offset) {
-        selection.range.focus.offset.increment(chars.length);
-      }
-      for (const uid of userIds) {
-        const slctn = users[uid];
-        if (slctn && slctn.range && slctn.ids[0] === node.id) {
-          if (uid !== userId) {
-            if (slctn.range.anchor.id === inlineId && slctn.range.anchor.offset.value > offset) {
-              slctn.range.anchor.offset.increment(chars.length);
-            }
-            if (slctn.range.focus.id === inlineId && slctn.range.focus.offset.value > offset) {
-              slctn.range.focus.offset.increment(chars.length);
-            }
+    const offset = selection.range.anchor.offset.value;
+    transform.inline.insertText(inline, offset, chars);
+
+    const userIds = Object.keys(users);
+    if (selection.range.anchor.id === inlineId && selection.range.anchor.offset.value >= offset) {
+      selection.range.anchor.offset.increment(chars.length);
+    }
+    if (selection.range.focus.id === selection.range.focus.id && selection.range.focus.offset.value >= offset) {
+      selection.range.focus.offset.increment(chars.length);
+    }
+    for (const uid of userIds) {
+      const slctn = users[uid];
+      if (slctn && slctn.range && slctn.ids[0] === node.id) {
+        if (uid !== userId) {
+          if (slctn.range.anchor.id === inlineId && slctn.range.anchor.offset.value > offset) {
+            slctn.range.anchor.offset.increment(chars.length);
+          }
+          if (slctn.range.focus.id === inlineId && slctn.range.focus.offset.value > offset) {
+            slctn.range.focus.offset.increment(chars.length);
           }
         }
       }
-    } else if (selection.range !== null && !sutils.isCollasped(selection)) {
-      actions.removeText(userId, doc);
-      actions.insertText(userId, doc, chars);
-    } else if (selection.range === null && selection.ids.length) {
-      // TODO
+    }
+  },
+
+  removeChar: (userId: string, doc: Doc): void => {
+    const users = doc.users;
+    const document = doc.document;
+    const selection: Selection = users[userId];
+
+    if (selection.range === null) return;
+
+    const node = traversal.node.findCurrentNode(selection, document);
+
+    if (node === null || node.inline === null) return;
+
+    const inlineId = selection.range.anchor.id;
+    const inline = traversal.inline.find(node, inlineId);
+
+    if (inline === null) return;
+
+    const offset = selection.range.anchor.offset.value;
+    transform.inline.removeText(inline, offset - 1, 1);
+
+    const userIds = Object.keys(users);
+    if (selection.range.anchor.id === inlineId && selection.range.anchor.offset.value >= offset) {
+      selection.range.anchor.offset.decrement(1);
+    }
+    if (selection.range.focus.id === selection.range.focus.id && selection.range.focus.offset.value >= offset) {
+      selection.range.focus.offset.decrement(1);
+    }
+    for (const uid of userIds) {
+      const slctn = users[uid];
+      if (slctn && slctn.range && slctn.ids[0] === node.id) {
+        if (uid !== userId) {
+          if (slctn.range.anchor.id === inlineId && slctn.range.anchor.offset.value > offset) {
+            slctn.range.anchor.offset.decrement(1);
+          }
+          if (slctn.range.focus.id === inlineId && slctn.range.focus.offset.value > offset) {
+            slctn.range.focus.offset.decrement(1);
+          }
+        }
+      }
     }
   },
 
@@ -132,162 +168,133 @@ export const actions = {
     const document = doc.document;
     const selection: Selection = users[userId];
 
-    if (selection.range !== null && sutils.isCollasped(selection)) {
-      const node = traversal.node.findCurrentNode(selection, document);
+    const node = traversal.node.findCurrentNode(selection, document);
 
-      if (node === null || node.inline === null) return;
+    if (node === null || node.inline === null) return;
 
-      const inlineId = selection.range.anchor.id;
-      const inline = traversal.inline.find(node, inlineId);
+    const { start, end } = getStartAndEnd(selection, node);
 
-      if (inline === null) return;
+    if (start === null || end === null) return;
 
-      const offset = selection.range.anchor.offset.value;
-      transform.inline.removeText(inline, offset - 1, 1);
+    if (start.id === end.id) {
+      const startInline = traversal.inline.find(node, start.id);
+
+      if (startInline === null) return;
+
+      for (let i = 0; i < node.inline.length; i += 1) {
+        const inline = node.inline[i];
+
+        const startInlineId = start.id;
+        const startOffset = start.offset.value;
+        const endInlineId = end.id;
+        const endOffset = end.offset.value;
+        transform.inline.removeText(inline, startOffset, endOffset - 1);
+
+        const size = endOffset - startOffset;
+        const userIds = Object.keys(users);
+        if (end.id === endInlineId && end.offset.value >= endOffset) {
+          end.offset.decrement(size);
+        }
+        for (const uid of userIds) {
+          const slctn = users[uid];
+          if (slctn && slctn.range && slctn.ids[0] === node.id) {
+            if (uid !== userId) {
+              if (slctn.range.anchor.id === startInlineId && slctn.range.anchor.offset.value > startOffset) {
+                slctn.range.anchor.offset.decrement(size);
+              }
+              if (slctn.range.focus.id === endInlineId && slctn.range.focus.offset.value > endOffset) {
+                slctn.range.focus.offset.decrement(size);
+              }
+            }
+          }
+        }
+      }
+    } else {
+      let isStarted = false;
+      const removedIds: string[] = [];
+
+      for (let i = 0; i < node.inline.length; i += 1) {
+        const inline = node.inline[i];
+
+        if (inline.id === start.id) {
+          isStarted = true;
+          const startInline = traversal.inline.find(node, start.id);
+          if (startInline) {
+            transform.inline.removeText(inline, start.offset.value, startInline.text.length - start.offset.value);
+          }
+        } else if (inline.id === end.id) {
+          isStarted = false;
+          const endInline = traversal.inline.find(node, end.id);
+          if (endInline) {
+            transform.inline.removeText(inline, 0, end.offset.value);
+          }
+        } else if (isStarted) {
+          removedIds.push(inline.id);
+          transform.inline.remove(node, inline);
+          i -= 1;
+        }
+      }
+
+      // Selections
+      const startId = start.id;
+      const startOffset = start.offset.value;
+      const endId = end.id;
+      const endOffset = end.offset.value;
+
+      end.id = start.id;
+      end.offset.increment(sutils.getOffset(endOffset, startOffset));
 
       const userIds = Object.keys(users);
-      if (selection.range.anchor.id === inlineId && selection.range.anchor.offset.value >= offset) {
-        selection.range.anchor.offset.decrement(1);
-      }
-      if (selection.range.focus.id === selection.range.focus.id && selection.range.focus.offset.value >= offset) {
-        selection.range.focus.offset.decrement(1);
-      }
       for (const uid of userIds) {
-        const slctn = users[uid];
-        if (slctn && slctn.range && slctn.ids[0] === node.id) {
-          if (uid !== userId) {
-            if (slctn.range.anchor.id === inlineId && slctn.range.anchor.offset.value > offset) {
-              slctn.range.anchor.offset.decrement(1);
-            }
-            if (slctn.range.focus.id === inlineId && slctn.range.focus.offset.value > offset) {
-              slctn.range.focus.offset.decrement(1);
-            }
-          }
-        }
-      }
-    } else if (selection.range !== null && !sutils.isCollasped(selection)) {
-      const node = traversal.node.findCurrentNode(selection, document);
+        if (uid !== userId) {
+          const slctn = users[uid];
 
-      if (node === null || node.inline === null) return;
+          if (slctn && slctn.range && slctn.ids[0] === node.id) {
+            const tmp = getStartAndEnd(slctn, node);
+            const collaboratorStart = tmp.start;
+            const collaboratorEnd = tmp.end;
 
-      const { start, end } = getStartAndEnd(selection, node);
-
-      if (start === null || end === null) return;
-
-      if (start.id === end.id) {
-        const startInline = traversal.inline.find(node, start.id);
-
-        if (startInline === null) return;
-
-        for (let i = 0; i < node.inline.length; i += 1) {
-          const inline = node.inline[i];
-
-          const startInlineId = start.id;
-          const startOffset = start.offset.value;
-          const endInlineId = end.id;
-          const endOffset = end.offset.value;
-          transform.inline.removeText(inline, startOffset, endOffset - 1);
-
-          const size = endOffset - startOffset;
-          const userIds = Object.keys(users);
-          if (end.id === endInlineId && end.offset.value >= endOffset) {
-            end.offset.decrement(size);
-          }
-          for (const uid of userIds) {
-            const slctn = users[uid];
-            if (slctn && slctn.range && slctn.ids[0] === node.id) {
-              if (uid !== userId) {
-                if (slctn.range.anchor.id === startInlineId && slctn.range.anchor.offset.value > startOffset) {
-                  slctn.range.anchor.offset.decrement(size);
-                }
-                if (slctn.range.focus.id === endInlineId && slctn.range.focus.offset.value > endOffset) {
-                  slctn.range.focus.offset.decrement(size);
-                }
+            if (collaboratorStart && collaboratorEnd) {
+              // For start
+              if (
+                (collaboratorStart.id === startId && collaboratorStart.offset.value > startOffset) ||
+                removedIds.includes(collaboratorStart.id)
+              ) {
+                collaboratorStart.id = startId;
+                collaboratorStart.offset.increment(sutils.getOffset(collaboratorStart.offset.value, startOffset));
               }
-            }
-          }
-        }
-      } else {
-        let isStarted = false;
-        const removedIds: string[] = [];
-
-        for (let i = 0; i < node.inline.length; i += 1) {
-          const inline = node.inline[i];
-
-          if (inline.id === start.id) {
-            isStarted = true;
-            const startInline = traversal.inline.find(node, start.id);
-            if (startInline) {
-              transform.inline.removeText(inline, start.offset.value, startInline.text.length - start.offset.value);
-            }
-          } else if (inline.id === end.id) {
-            isStarted = false;
-            const endInline = traversal.inline.find(node, end.id);
-            if (endInline) {
-              transform.inline.removeText(inline, 0, end.offset.value);
-            }
-          } else if (isStarted) {
-            removedIds.push(inline.id);
-            transform.inline.remove(node, inline);
-            i -= 1;
-          }
-        }
-
-        // Selections
-        const startId = start.id;
-        const startOffset = start.offset.value;
-        const endId = end.id;
-        const endOffset = end.offset.value;
-
-        end.id = start.id;
-        end.offset.increment(sutils.getOffset(endOffset, startOffset));
-
-        const userIds = Object.keys(users);
-        for (const uid of userIds) {
-          if (uid !== userId) {
-            const slctn = users[uid];
-
-            if (slctn && slctn.range && slctn.ids[0] === node.id) {
-              const tmp = getStartAndEnd(slctn, node);
-              const collaboratorStart = tmp.start;
-              const collaboratorEnd = tmp.end;
-
-              if (collaboratorStart && collaboratorEnd) {
-                // For start
-                if (
-                  (collaboratorStart.id === startId && collaboratorStart.offset.value > startOffset) ||
-                  removedIds.includes(collaboratorStart.id)
-                ) {
-                  collaboratorStart.id = startId;
-                  collaboratorStart.offset.increment(sutils.getOffset(collaboratorStart.offset.value, startOffset));
-                }
-                // For end
-                if (removedIds.includes(collaboratorEnd.id)) {
-                  collaboratorEnd.id = startId;
-                  collaboratorEnd.offset.increment(sutils.getOffset(collaboratorEnd.offset.value, startOffset));
-                } else if (collaboratorEnd.id === endId && collaboratorEnd.offset.value > endOffset) {
-                  collaboratorEnd.offset.decrement(endOffset);
-                }
+              // For end
+              if (removedIds.includes(collaboratorEnd.id)) {
+                collaboratorEnd.id = startId;
+                collaboratorEnd.offset.increment(sutils.getOffset(collaboratorEnd.offset.value, startOffset));
+              } else if (collaboratorEnd.id === endId && collaboratorEnd.offset.value > endOffset) {
+                collaboratorEnd.offset.decrement(endOffset);
               }
             }
           }
         }
       }
-    } else if (selection.range === null && selection.ids.length) {
-      const nodes = traversal.node.findCurrentNodes(selection, document);
+    }
+  },
 
-      for (const node of nodes) {
-        if (node !== null && node.object === 'item') {
-          transform.node.remove(document, node);
-          if (node.next) {
-            selection.ids = [node.next];
-          } else if (node.prev) {
-            selection.ids = [node.prev];
-          } else if (node.parent) {
-            selection.ids = [node.parent];
-          } else {
-            selection.ids = [];
-          }
+  removeItems: (userId: string, doc: Doc): void => {
+    const users = doc.users;
+    const document = doc.document;
+    const selection: Selection = users[userId];
+
+    const nodes = traversal.node.findCurrentNodes(selection, document);
+
+    for (const node of nodes) {
+      if (node !== null && node.object === 'item') {
+        transform.node.remove(document, node);
+        if (node.next) {
+          selection.ids = [node.next];
+        } else if (node.prev) {
+          selection.ids = [node.prev];
+        } else if (node.parent) {
+          selection.ids = [node.parent];
+        } else {
+          // TODO: 「項目を追加する」を適用
         }
       }
     }
