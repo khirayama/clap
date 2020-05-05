8ff0d19fb265f6618b55d8a56313d8468c41a523
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-05-05 16:08:17 +0900

    Split testcases for usecases

---
+++ CRDT/src/usecases.input.spec.ts
@@ -0,0 +1,218 @@
+import * as assert from 'power-assert';
+
+import { usecases } from './usecases';
+import { CRDTDocument } from './CRDTDocument';
+import { utils as sutils } from './selection';
+import { createSampleData, toLooseJSON } from './testutils';
+
+let user: { id: string };
+let member: { id: string };
+let userDoc: CRDTDocument;
+let memberDoc: CRDTDocument;
+
+beforeEach(() => {
+  const result = createSampleData();
+  user = result.user;
+  member = result.member;
+  userDoc = result.userDoc;
+  memberDoc = result.memberDoc;
+});
+
+describe('.input()', () => {
+  describe('選択範囲が閉じている状態で', () => {
+    describe('「あいうえお」と挿入したとき', () => {
+      it('文字列が挿入されて選択範囲位置が文字数分後ろへ移動していること', () => {
+        const expectedDoc = toLooseJSON(userDoc);
+        const inlineText = expectedDoc.doc.document.nodes[0].inline[0];
+        const userSelection = expectedDoc.doc.users[user.id];
+        inlineText.text = ['あ', 'い', 'う', 'え', 'お'];
+        userSelection.range.anchor.offset = 5;
+        userSelection.range.focus.offset = 5;
+
+        userDoc.change((doc) => {
+          usecases.input(user.id, doc, ['あ', 'い', 'う', 'え', 'お']);
+        });
+
+        assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
+      });
+
+      it('文字列が挿入されて共同編集者の選択範囲始点が文字数分後ろへ移動していること', () => {
+        const expectedDoc = toLooseJSON(userDoc);
+        const inlineText = expectedDoc.doc.document.nodes[0].inline[0];
+        const userSelection = expectedDoc.doc.users[user.id];
+        const memberSelection = expectedDoc.doc.users[member.id];
+        inlineText.text = ['1', 'あ', 'い', 'う', 'え', 'お', '2', '3'];
+        userSelection.range.anchor.offset = 6;
+        userSelection.range.focus.offset = 6;
+        memberSelection.range.anchor.offset = 7;
+        memberSelection.range.focus.offset = 7;
+
+        userDoc.change((doc) => {
+          usecases.input(user.id, doc, ['1', '2', '3']);
+        });
+        memberDoc.merge(userDoc);
+        memberDoc.change((doc) => {
+          const range = doc.users[member.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(sutils.getOffset(range.anchor.offset.value, 2));
+            range.focus.offset.increment(sutils.getOffset(range.focus.offset.value, 2));
+          }
+        });
+        userDoc.merge(memberDoc);
+        userDoc.change((doc) => {
+          const range = doc.users[user.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(sutils.getOffset(range.anchor.offset.value, 1));
+            range.focus.offset.increment(sutils.getOffset(range.focus.offset.value, 1));
+          }
+          usecases.input(user.id, doc, ['あ', 'い', 'う', 'え', 'お']);
+        });
+
+        assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
+      });
+
+      it('文字列が挿入されて共同編集者の選択範囲終点が文字数分後ろへ移動していること', () => {
+        const expectedDoc = toLooseJSON(userDoc);
+        const inlineText = expectedDoc.doc.document.nodes[0].inline[0];
+        const userSelection = expectedDoc.doc.users[user.id];
+        const memberSelection = expectedDoc.doc.users[member.id];
+        inlineText.text = ['1', 'あ', 'い', 'う', 'え', 'お', '2', '3'];
+        userSelection.range.anchor.offset = 6;
+        userSelection.range.focus.offset = 6;
+        memberSelection.range.anchor.offset = 1;
+        memberSelection.range.focus.offset = 7;
+
+        userDoc.change((doc) => {
+          usecases.input(user.id, doc, ['1', '2', '3']);
+        });
+        memberDoc.merge(userDoc);
+        memberDoc.change((doc) => {
+          const range = doc.users[member.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(sutils.getOffset(range.anchor.offset.value, 1));
+            range.focus.offset.increment(sutils.getOffset(range.focus.offset.value, 2));
+          }
+        });
+        userDoc.merge(memberDoc);
+        userDoc.change((doc) => {
+          const range = doc.users[user.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(sutils.getOffset(range.anchor.offset.value, 1));
+            range.focus.offset.increment(sutils.getOffset(range.focus.offset.value, 1));
+          }
+          usecases.input(user.id, doc, ['あ', 'い', 'う', 'え', 'お']);
+        });
+
+        assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
+      });
+
+      it('文字列が挿入されて共同編集者の選択範囲の始点と終点が逆位置の場合でも始点が文字数分後ろへ移動していること', () => {
+        const expectedDoc = toLooseJSON(userDoc);
+        const inlineText = expectedDoc.doc.document.nodes[0].inline[0];
+        const userSelection = expectedDoc.doc.users[user.id];
+        const memberSelection = expectedDoc.doc.users[member.id];
+        inlineText.text = ['1', 'あ', 'い', 'う', 'え', 'お', '2', '3'];
+        userSelection.range.anchor.offset = 6;
+        userSelection.range.focus.offset = 6;
+        memberSelection.range.anchor.offset = 7;
+        memberSelection.range.focus.offset = 0;
+
+        userDoc.change((doc) => {
+          usecases.input(user.id, doc, ['1', '2', '3']);
+        });
+        memberDoc.merge(userDoc);
+        memberDoc.change((doc) => {
+          const range = doc.users[member.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(sutils.getOffset(range.anchor.offset.value, 2));
+            range.focus.offset.increment(sutils.getOffset(range.focus.offset.value, 0));
+          }
+        });
+        userDoc.merge(memberDoc);
+        userDoc.change((doc) => {
+          const range = doc.users[user.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(sutils.getOffset(range.anchor.offset.value, 1));
+            range.focus.offset.increment(sutils.getOffset(range.focus.offset.value, 1));
+          }
+          usecases.input(user.id, doc, ['あ', 'い', 'う', 'え', 'お']);
+        });
+
+        assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
+      });
+    });
+  });
+  describe('選択範囲が開いている状態で', () => {
+    describe('「あいうえお」と挿入したとき', () => {
+      it('選択範囲を削除が適用され、文字挿入が行われていること', () => {
+        const expectedDoc = toLooseJSON(userDoc);
+        const inlineText = expectedDoc.doc.document.nodes[0].inline[0];
+        const userSelection = expectedDoc.doc.users[user.id];
+        const memberSelection = expectedDoc.doc.users[member.id];
+        inlineText.text = ['1', 'あ', 'い', 'う', 'え', 'お', '4'];
+        userSelection.range.anchor.offset = 6;
+        userSelection.range.focus.offset = 6;
+        memberSelection.range.anchor.offset = 0;
+        memberSelection.range.focus.offset = 7;
+
+        userDoc.change((doc) => {
+          usecases.input(user.id, doc, ['1', '2', '3', '4']);
+        });
+        memberDoc.merge(userDoc);
+        memberDoc.change((doc) => {
+          const range = doc.users[member.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(sutils.getOffset(range.anchor.offset.value, 0));
+            range.focus.offset.increment(sutils.getOffset(range.focus.offset.value, 4));
+          }
+        });
+        userDoc.merge(memberDoc);
+        userDoc.change((doc) => {
+          const range = doc.users[user.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(sutils.getOffset(range.anchor.offset.value, 1));
+            range.focus.offset.increment(sutils.getOffset(range.focus.offset.value, 3));
+          }
+          usecases.input(user.id, doc, ['あ', 'い', 'う', 'え', 'お']);
+        });
+
+        assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
+      });
+
+      it('共同編集者選択範囲が逆位置でも選択範囲を削除が適用され、文字挿入が行われていること', () => {
+        const expectedDoc = toLooseJSON(userDoc);
+        const inlineText = expectedDoc.doc.document.nodes[0].inline[0];
+        const userSelection = expectedDoc.doc.users[user.id];
+        const memberSelection = expectedDoc.doc.users[member.id];
+        inlineText.text = ['1', 'あ', 'い', 'う', 'え', 'お', '4'];
+        userSelection.range.anchor.offset = 6;
+        userSelection.range.focus.offset = 6;
+        memberSelection.range.anchor.offset = 7;
+        memberSelection.range.focus.offset = 0;
+
+        userDoc.change((doc) => {
+          usecases.input(user.id, doc, ['1', '2', '3', '4']);
+        });
+        memberDoc.merge(userDoc);
+        memberDoc.change((doc) => {
+          const range = doc.users[member.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(sutils.getOffset(range.anchor.offset.value, 4));
+            range.focus.offset.increment(sutils.getOffset(range.focus.offset.value, 0));
+          }
+        });
+        userDoc.merge(memberDoc);
+        userDoc.change((doc) => {
+          const range = doc.users[user.id].range;
+          if (range !== null) {
+            range.anchor.offset.increment(sutils.getOffset(range.anchor.offset.value, 1));
+            range.focus.offset.increment(sutils.getOffset(range.focus.offset.value, 3));
+          }
+          usecases.input(user.id, doc, ['あ', 'い', 'う', 'え', 'お']);
+        });
+
+        assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
+      });
+    });
+  });
+});
