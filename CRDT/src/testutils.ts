10ca22462b6f4e92ac3b399e3768480817e2c874
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-05-05 17:33:03 +0900

    Update sample document structure

--- CRDT/src/testutils.ts
+++ CRDT/src/testutils.ts
@@ -1,9 +1,72 @@
 import { factory } from './factory';
 import { CRDTDocument } from './CRDTDocument';
+import { transform } from './transform';
 
 export function toLooseJSON(obj: any) {
   // FYI: AutomergeのCounterが、{ value: number }からnumberに変わってしまう点に注意
   return JSON.parse(JSON.stringify(obj));
+}
+
+/*
+ Selection
+  user
+  member
+ Document
+  Paragraph1
+    - ABCDEFGHI
+    Paragraph2
+      - ABCDEFGHI
+      Paragraph3
+        - ABC
+        - DEF
+        - GHI
+  Paragraph4
+    - ABCDEFGHI
+  Paragraph5
+    - ABCDEFGHI
+    Paragraph6
+      - ABCDEFGHI
+      Paragraph7
+        - ABC
+        - DEF
+        - GHI
+  Paragraph8
+    - ABCDEFGHI
+  Paragraph9
+    - ABCDEFGHI
+    Paragraph10
+      - ABCDEFGHI
+      Paragraph11
+        - ABC
+        - DEF
+        - GHI
+    Paragraph12
+      - ABCDEFGHI
+*/
+
+function createInlineText1() {
+  const inlineText = factory.inline.createInlineText();
+  inlineText.text = 'ABCDEFGHI'.split('');
+  return inlineText;
+}
+
+function createInlineText2() {
+  const inlineText = factory.inline.createInlineText();
+  inlineText.text = 'ABC'.split('');
+  return inlineText;
+}
+
+function createInlineText3() {
+  const inlineText = factory.inline.createInlineText();
+  inlineText.text = 'DEF'.split('');
+  inlineText.marks = ['bold'];
+  return inlineText;
+}
+
+function createInlineText4() {
+  const inlineText = factory.inline.createInlineText();
+  inlineText.text = 'GHI'.split('');
+  return inlineText;
 }
 
 export function createSampleData() {
@@ -13,26 +76,81 @@ export function createSampleData() {
   const member = {
     id: factory.uuid(),
   };
+
   const userDoc = new CRDTDocument(user.id);
   const memberDoc = new CRDTDocument(member.id, userDoc.save());
-  memberDoc.change((doc) => {
-    const selection = factory.selection.createSelection();
-    doc.users[member.id] = selection;
+
+  userDoc.change((doc) => {
+    const document = doc.document;
+    transform.node.remove(document, document.nodes[0]);
+
+    const paragraph1 = factory.node.createParagraphNode();
+    paragraph1.inline.push(createInlineText1());
+    const paragraph2 = factory.node.createParagraphNode();
+    paragraph2.inline.push(createInlineText1());
+    const paragraph3 = factory.node.createParagraphNode();
+    paragraph3.inline.push(createInlineText2());
+    paragraph3.inline.push(createInlineText3());
+    paragraph3.inline.push(createInlineText4());
+    const paragraph4 = factory.node.createParagraphNode();
+    paragraph4.inline.push(createInlineText1());
+    const paragraph5 = factory.node.createParagraphNode();
+    paragraph5.inline.push(createInlineText1());
+    const paragraph6 = factory.node.createParagraphNode();
+    paragraph6.inline.push(createInlineText1());
+    const paragraph7 = factory.node.createParagraphNode();
+    paragraph7.inline.push(createInlineText2());
+    paragraph7.inline.push(createInlineText3());
+    paragraph7.inline.push(createInlineText4());
+    const paragraph8 = factory.node.createParagraphNode();
+    paragraph8.inline.push(createInlineText1());
+    const paragraph9 = factory.node.createParagraphNode();
+    paragraph9.inline.push(createInlineText1());
+    const paragraph10 = factory.node.createParagraphNode();
+    paragraph10.inline.push(createInlineText1());
+    const paragraph11 = factory.node.createParagraphNode();
+    paragraph11.inline.push(createInlineText2());
+    paragraph11.inline.push(createInlineText3());
+    paragraph11.inline.push(createInlineText4());
+    const paragraph12 = factory.node.createParagraphNode();
+    paragraph12.inline.push(createInlineText1());
+
+    transform.node.append(paragraph2, paragraph3);
+    transform.node.append(paragraph1, paragraph2);
+    transform.node.append(document, paragraph1);
+    transform.node.append(document, paragraph4);
+    transform.node.append(paragraph6, paragraph7);
+    transform.node.append(paragraph5, paragraph6);
+    transform.node.append(document, paragraph5);
+    transform.node.append(document, paragraph8);
+    transform.node.append(paragraph10, paragraph11);
+    transform.node.append(paragraph9, paragraph10);
+    transform.node.append(paragraph9, paragraph12);
+    transform.node.append(document, paragraph9);
   });
-  userDoc.merge(new CRDTDocument(member.id, memberDoc.save()));
+  memberDoc.merge(new CRDTDocument(user.id, userDoc.save()));
+
+  userDoc.change((doc) => {
+    const document = doc.document;
+    const selection = doc.users[user.id];
+    const firstNode = document.nodes[0];
+
+    selection.ids.splice(0, selection.ids.length, firstNode.id);
+    selection.range = factory.selection.createRange(firstNode.inline[0].id, firstNode.inline[0].text.length);
+  });
   memberDoc.merge(new CRDTDocument(user.id, userDoc.save()));
 
   memberDoc.change((doc) => {
     const document = doc.document;
-    const selection = doc.users[member.id];
+    const selection = factory.selection.createSelection();
     const firstNode = document.nodes[0];
 
-    selection.ids.push(firstNode.id);
-    selection.range = factory.selection.createRange(firstNode.inline[0].id, 0);
-  });
+    selection.ids.splice(0, selection.ids.length, firstNode.id);
+    selection.range = factory.selection.createRange(firstNode.inline[0].id, firstNode.inline[0].text.length);
 
+    doc.users[member.id] = selection;
+  });
   userDoc.merge(new CRDTDocument(member.id, memberDoc.save()));
-  memberDoc.merge(new CRDTDocument(user.id, userDoc.save()));
 
   return {
     user,
