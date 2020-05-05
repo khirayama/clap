14cd07996c2ebdb143318420d1278a5d7cc2b1a7
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-05-05 16:08:08 +0900

    Update usecases

--- CRDT/src/usecases.ts
+++ CRDT/src/usecases.ts
@@ -29,17 +29,41 @@ export const usecases = {
   outdent: () => {},
   remove: (userId: string, doc: Doc) => {
     const users = doc.users;
+    const document = doc.document;
     const selection: Selection = users[userId];
 
     if (selection.range !== null && sutils.isCollasped(selection)) {
-      // TODO: 編集者選択範囲始点が項目のインライン先頭の先頭だった場合
-      actions.removeChar(userId, doc);
+      const node = traversal.node.findCurrentNode(selection, document);
+      if (
+        node !== null &&
+        node.inline !== null &&
+        node.object === 'item' &&
+        selection.range.anchor.id === node.inline[0].id &&
+        selection.range.anchor.offset.value === 0
+      ) {
+        // TODO: 編集者選択範囲始点が項目のインライン先頭の先頭だった場合
+        if (node.type !== 'paragraph') {
+          // TODO: 「Actions 項目タイプを変更する」で段落項目へ変換を適用
+        } else if (node.parent !== node.document) {
+          // TODO: 「Actions アウトデントする」を適用
+        } else if (node.parent === node.document) {
+          /* TODO:
+        - 上項目がインラインを持つ場合
+          - 上項目とインラインとして結合する
+        - 上項目がインラインを持たない場合
+          - 「Actions 項目を削除する」で上項目を削除を適用
+          - 「Actions 項目を削除したときの後処理」を適用
+            */
+        }
+      } else {
+        actions.removeChar(userId, doc);
+      }
     } else if (selection.range !== null && !sutils.isCollasped(selection)) {
       actions.removeText(userId, doc);
-      // TODO: 「Actions 文字を削除した場合の後処理」を適用する
+      actions.postprocessTextDeletion(userId, doc);
     } else if (selection.range === null && selection.ids.length) {
       actions.removeItems(userId, doc);
-      // TODO: 「Actions 項目を削除したときの後処理」を適用
+      actions.postprocessItemDeletion(userId, doc);
     }
   },
 };
