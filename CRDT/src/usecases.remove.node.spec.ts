16870cd593f997a382cd4f9c0f61f75b2ea6c291
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-05-06 21:06:47 +0900

    Update test cases

--- CRDT/src/usecases.remove.node.spec.ts
+++ CRDT/src/usecases.remove.node.spec.ts
@@ -190,7 +190,64 @@ describe('削除操作', () => {
       });
     });
     describe(`編集者選択範囲が${nodePatterns.b}`, () => {
-      it.skip('', () => {});
+      describe(`共同編集者選択範囲が${nodePatterns.a}`, () => {
+        it('複数項目が削除され、編集者と共同編集者の選択範囲が次項目へ移動していること', () => {
+          userDoc.change((doc) => {
+            doc.users[user.id].ids = [doc.document.nodes[1].id, doc.document.nodes[2].id, doc.document.nodes[3].id];
+            doc.users[user.id].range = null;
+          });
+          memberDoc.merge(userDoc);
+          memberDoc.change((doc) => {
+            doc.users[member.id].ids = [doc.document.nodes[1].id];
+            doc.users[member.id].range = null;
+          });
+          userDoc.merge(memberDoc);
+
+          const expectedDoc = toLooseJSON(userDoc);
+          const nodes = expectedDoc.doc.document.nodes;
+          nodes[0].next = nodes[4].id;
+          nodes[4].prev = nodes[0].id;
+          expectedDoc.doc.users[user.id].ids = [nodes[4].id];
+          expectedDoc.doc.users[member.id].ids = [nodes[4].id];
+          nodes.splice(1, 3);
+
+          userDoc.change((doc) => {
+            usecases.remove(user.id, doc);
+          });
+
+          assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
+        });
+        it.skip('複数項目が削除され、編集者と共同編集者の選択範囲が前項目へ移動していること', () => {});
+        it.skip('複数項目が削除され、編集者と共同編集者の選択範囲が親項目へ移動していること', () => {});
+        it.skip('複数項目が削除され、編集者と共同編集者の選択範囲がなくなっていること', () => {});
+      });
+      describe(`共同編集者選択範囲が${nodePatterns.b}`, () => {
+        describe('編集者と共同編集者が同じ選択範囲の場合', () => {
+          it.skip('複数項目が削除され、編集者と共同編集者の選択範囲が次項目に移動していること', () => {});
+          it.skip('複数項目が削除され、編集者と共同編集者の選択範囲が前項目に移動していること', () => {});
+          it.skip('複数項目が削除され、編集者と共同編集者の選択範囲が親項目に移動していること', () => {});
+          it.skip('複数項目が削除され、編集者と共同編集者の選択項目がなくなっていること', () => {});
+        });
+        describe('編集者選択範囲前に共同編集者選択範囲がある場合', () => {
+          it.skip('複数項目が削除され、編集者の選択範囲が次項目に移動し、共同編集者選択項目に影響がないこと', () => {});
+          it.skip('複数項目が削除され、編集者の選択範囲が前項目に移動し、共同編集者選択項目に影響がないこと', () => {});
+        });
+        describe('編集者選択範囲内に共同編集者選択範囲がある場合', () => {
+          it.skip('複数項目が削除され、編集者と共同編集者の選択範囲が次項目に移動していること', () => {});
+          it.skip('複数項目が削除され、編集者と共同編集者の選択範囲が前項目に移動していること', () => {});
+          it.skip('複数項目が削除され、編集者と共同編集者の選択範囲が親項目に移動していること', () => {});
+          it.skip('複数項目が削除され、編集者と共同編集者の選択項目がなくなっていること', () => {});
+        });
+        describe('編集者選択範囲と共同編集者選択範囲に一部重複がある場合', () => {
+          it.skip('複数項目が削除され、編集者の選択範囲が次項目に移動し、共同編集者選択項目の重複項目が削除されていること', () => {});
+          it.skip('複数項目が削除され、編集者の選択範囲が前項目に移動し、共同編集者選択項目の重複項目が削除されていること', () => {});
+        });
+        describe('編集者選択範囲後に共同編集者選択範囲がある場合', () => {
+          it.skip('複数項目が削除され、編集者の選択範囲が次項目に移動し、共同編集者選択項目に影響がないこと', () => {});
+          it.skip('複数項目が削除され、編集者の選択範囲が前項目に移動し、共同編集者選択項目に影響がないこと', () => {});
+          it.skip('', () => {});
+        });
+      });
     });
   });
 });
