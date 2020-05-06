34db66b7e8472ef3143e99f8b9a715dd9914d7d0
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-05-06 18:29:49 +0900

    Support new test cases

--- CRDT/src/usecases.remove.node.spec.ts
+++ CRDT/src/usecases.remove.node.spec.ts
@@ -72,7 +72,7 @@ describe('削除操作', () => {
           assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
         });
 
-        it('項目が削除され、編集者と共同編集者の選択範囲が前項目へ移動していること', () => {
+        it('項目が削除され、編集者と共同編集者の選択範囲が親項目へ移動していること', () => {
           userDoc.change((doc) => {
             doc.users[user.id].ids = [doc.document.nodes[0].nodes[0].nodes[0].id];
             doc.users[user.id].range = null;
@@ -156,11 +156,37 @@ describe('削除操作', () => {
           assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
         });
 
-        it.skip('項目が削除され、選択範囲が前項目へ移動していること', () => {});
+        it('項目が削除され、編集者選択範囲が前項目へ移動し、共同編集者選択項目から項目が取り除かれていること', () => {
+          userDoc.change((doc) => {
+            doc.users[user.id].ids = [doc.document.nodes[4].id];
+            doc.users[user.id].range = null;
+          });
+          memberDoc.merge(userDoc);
+          memberDoc.change((doc) => {
+            doc.users[member.id].range = null;
+            doc.users[member.id].ids = [
+              doc.document.nodes[0].id,
+              doc.document.nodes[1].id,
+              doc.document.nodes[2].id,
+              doc.document.nodes[3].id,
+              doc.document.nodes[4].id,
+            ];
+          });
+          userDoc.merge(memberDoc);
 
-        it.skip('項目が削除され、選択範囲が親項目へ移動していること', () => {});
+          const expectedDoc = toLooseJSON(userDoc);
+          const nodes = expectedDoc.doc.document.nodes;
+          nodes[3].next = null;
+          expectedDoc.doc.users[user.id].ids = [nodes[3].id];
+          expectedDoc.doc.users[member.id].ids = [nodes[0].id, nodes[1].id, nodes[2].id, nodes[3].id];
+          nodes.splice(4, 1);
 
-        it.skip('項目が削除され、選択範囲がなくなっていること', () => {});
+          userDoc.change((doc) => {
+            usecases.remove(user.id, doc);
+          });
+
+          assert.deepEqual(toLooseJSON(userDoc), expectedDoc);
+        });
       });
     });
     describe(`編集者選択範囲が${nodePatterns.b}`, () => {
