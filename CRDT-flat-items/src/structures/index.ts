0419572cde29bee5247e2f26bb9e0aee10981b4e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-05-21 08:11:39 +0900

    Create index file for structure

---
+++ CRDT-flat-items/src/structures/index.ts
@@ -0,0 +1,5 @@
+export { Document } from './document';
+export { Item, ParagraphItem, Heading1Item, HorizontalRuleItem } from './item';
+export { Inline, InlineText, Decoration } from './inline';
+export { Selection, Range } from './selection';
+export { utils } from './utils';
