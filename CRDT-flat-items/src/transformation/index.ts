cadc2bc324e60efce59765eabbe465dd05bcc05e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-05-24 13:33:39 +0900

    Update transformation

--- CRDT-flat-items/src/transformation/index.ts
+++ CRDT-flat-items/src/transformation/index.ts
@@ -1,9 +1,10 @@
 // factory, traversal
 import { factory } from '../factory';
-import { traversal } from '../traversal';
+// import { traversal } from '../traversal';
 
 import {
   Document,
+  SuperItem,
   Item,
   ParagraphItem,
   Heading1Item,
@@ -20,32 +21,11 @@ import {
  * remove: ドキュメントと削除したい要素を与え、自身を削除する。 https://developer.mozilla.org/ja/docs/Web/API/ChildNode/remove
  */
 export function transformation(document: Document) {
-  const traverse = traversal(document);
-
   const transform = {
     item: {
-      append: (parentItem: Item, item: Item): void => {
-        for (let i = 0; i < document.items.length; i += 1) {
-          const itm = document.items[i];
-          if (itm.id === item.id) {
-            document.items.splice(i, 1);
-          }
-        }
-
-        for (let i = 0; i < document.items.length; i += 1) {
-          const itm = document.items[i];
-          if (itm.id === parentItem.id) {
-            while (document.items[i + 1] && parentItem.indent < document.items[i + 1].indent) {
-              i += 1;
-            }
-            item.indent.increment(sutils.getOffset(item.indent.value, parentItem.indent.value + 1));
-            document.items.splice(i, 0, item);
-            break;
-          }
-        }
-      },
-
       after: (prevItem: Item, item: Item): void => {
+        transform.item.remove(item);
+
         for (let i = 0; i < document.items.length; i += 1) {
           const itm = document.items[i];
           if (itm.id === prevItem.id) {
@@ -66,107 +46,62 @@ export function transformation(document: Document) {
         }
       },
 
-      appendInline: (parentNode: ItemNode, inline: Inline): void => {
-        if (parentNode.inline === null) return;
+      appendInline: (item: Item, inline: Inline): void => {
+        if (item.inline === null) return;
 
-        inline.parent = parentNode.id;
-        parentNode.inline.push(inline);
+        inline.parent = item.id;
+        item.inline.push(inline);
       },
 
-      turnInto: (node: ItemNode, itemType: SuperNode['type']): void => {
+      turnInto: (item: Item, itemType: SuperItem['type']): void => {
         switch (itemType) {
           case 'paragraph': {
-            const tmp = node as ParagraphNode;
+            const tmp = item as ParagraphItem;
             tmp.type = 'paragraph';
-            if (node.inline === null) {
+            if (item.inline === null) {
               tmp.inline = [];
               const inline = factory.inline.createInlineText();
-              transform.node.appendInline(tmp, inline);
-            }
-            if (node.nodes === null) {
-              tmp.nodes = [];
+              transform.item.appendInline(tmp, inline);
             }
             break;
           }
           case 'heading1': {
-            const tmp = node as Heading1Node;
+            const tmp = item as Heading1Item;
             tmp.type = 'heading1';
-            if (node.inline === null) {
+            if (item.inline === null) {
               tmp.inline = [];
               const inline = factory.inline.createInlineText();
-              transform.node.appendInline(tmp, inline);
-            }
-            if (node.nodes !== null) {
-              for (let i = 0; i < node.nodes.length; i += 1) {
-                transform.node.outdent(node.nodes[i]);
-              }
-              tmp.nodes = null;
+              transform.item.appendInline(tmp, inline);
             }
             break;
           }
           case 'horizontal-rule': {
-            const tmp = node as HorizontalRuleNode;
+            const tmp = item as HorizontalRuleItem;
             tmp.type = 'horizontal-rule';
-            if (node.inline !== null) {
+            if (item.inline !== null) {
               tmp.inline = null;
-            }
-            if (node.nodes !== null) {
-              for (let i = 0; i < node.nodes.length; i += 1) {
-                transform.node.outdent(node.nodes[i]);
-              }
-              tmp.nodes = null;
             }
             break;
           }
         }
       },
 
-      indent: (node: ItemNode): void => {
-        if (node.prev === null) return;
-
-        const upperNode = traverse.node.find(node.prev);
-
-        if (upperNode === null) return;
-
-        transform.node.append(upperNode, node);
-
-        if (node.nodes === null) return;
-
-        for (const nd of node.nodes) {
-          transform.node.append(upperNode, nd);
-        }
+      indent: (item: Item): void => {
+        item.indent.increment(1);
       },
 
-      outdent: (node: ItemNode): void => {
-        if (node.parent === null) return;
-
-        const parentNode = traverse.node.find(node.parent);
-
-        if (parentNode === null || parentNode.object === 'document') return;
-
-        if (parentNode.nodes !== null && node.nodes !== null) {
-          let isDowner = false;
-          for (let i = 0; i < parentNode.nodes.length; i += 1) {
-            const nd = parentNode.nodes[i];
-            if (nd.id === node.id) {
-              isDowner = true;
-            } else if (isDowner) {
-              transform.node.append(node, nd);
-            }
-          }
-        }
-
-        transform.node.after(parentNode, node);
+      outdent: (item: Item): void => {
+        item.indent.decrement(1);
       },
     },
 
     inline: {
-      remove: (node: ItemNode, inline: Inline): void => {
-        if (node.inline) {
-          for (let i = 0; i < node.inline.length; i += 1) {
-            const inl = node.inline[i];
+      remove: (item: Item, inline: Inline): void => {
+        if (item.inline) {
+          for (let i = 0; i < item.inline.length; i += 1) {
+            const inl = item.inline[i];
             if (inl.id === inline.id) {
-              node.inline.splice(i, 1);
+              item.inline.splice(i, 1);
               return;
             }
           }
