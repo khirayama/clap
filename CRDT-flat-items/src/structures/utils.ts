4eff3d0812ad913b87c46e2d766466e659c6b5d7
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-05-24 14:40:52 +0900

    Update utils for structure

--- CRDT-flat-items/src/structures/utils.ts
+++ CRDT-flat-items/src/structures/utils.ts
@@ -1,8 +1,20 @@
-import { Selection } from './selection';
+import { Document } from './Document';
+import { Selection } from './Selection';
 
 export const utils = {
-  getOffset: (curOffset: number, offset: number) => {
+  getOffset: (curOffset: number, offset: number): number => {
     return offset - curOffset;
+  },
+
+  item: {
+    getIndex: (document: Document, id: string): number => {
+      for (let i = 0; i < document.items.length; i += 1) {
+        if (document.items[i].id === id) {
+          return i;
+        }
+      }
+      return -1;
+    },
   },
 
   selection: {
