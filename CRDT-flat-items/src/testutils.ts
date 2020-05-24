d04b611a44daf37886841dadf406bcc18ca01b39
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-05-24 16:11:17 +0900

    Fix testutils

--- CRDT-flat-items/src/testutils.ts
+++ CRDT-flat-items/src/testutils.ts
@@ -1,7 +1,7 @@
 import { factory } from './factory';
-import { CRDTDocument } from './CRDTDocument';
+import { BoardHandler } from './BoardHandler';
 import { transformation } from './transformation';
-import { ParagraphNode, Heading1Node } from './node';
+import { ParagraphItem, Heading1Item } from './structures';
 
 export function toLooseJSON(obj: any) {
   // FYI: AutomergeのCounterが、{ value: number }からnumberに変わってしまう点に注意
@@ -80,88 +80,96 @@ export function createSampleData() {
     id: factory.uuid(),
   };
 
-  const userDoc = new CRDTDocument(user.id);
-  const memberDoc = new CRDTDocument(member.id, userDoc.save());
+  const userDoc = new BoardHandler(user.id);
+  const memberDoc = new BoardHandler(member.id, userDoc.save());
 
   userDoc.change((doc) => {
     const document = doc.document;
 
     const transform = transformation(document);
 
-    transform.node.remove(document.nodes[0]);
+    transform.item.remove(document.items[0]);
 
-    const paragraph1 = factory.node.createParagraphNode();
-    transform.node.appendInline(paragraph1, createInlineText1());
-    const paragraph2 = factory.node.createParagraphNode();
-    transform.node.appendInline(paragraph2, createInlineText1());
-    const paragraph3 = factory.node.createParagraphNode();
-    transform.node.appendInline(paragraph3, createInlineText2());
-    transform.node.appendInline(paragraph3, createInlineText3());
-    transform.node.appendInline(paragraph3, createInlineText4());
-    const paragraph4 = factory.node.createParagraphNode();
-    transform.node.appendInline(paragraph4, createInlineText1());
-    const paragraph5 = factory.node.createParagraphNode();
-    transform.node.appendInline(paragraph5, createInlineText1());
-    const paragraph6 = factory.node.createParagraphNode();
-    transform.node.appendInline(paragraph6, createInlineText1());
-    const paragraph7 = factory.node.createParagraphNode();
-    transform.node.appendInline(paragraph7, createInlineText1());
-    const paragraph8 = factory.node.createParagraphNode();
-    transform.node.appendInline(paragraph8, createInlineText2());
-    transform.node.appendInline(paragraph8, createInlineText3());
-    transform.node.appendInline(paragraph8, createInlineText4());
-    const paragraph9 = factory.node.createParagraphNode();
-    transform.node.appendInline(paragraph9, createInlineText1());
-    const paragraph10 = factory.node.createParagraphNode();
-    transform.node.appendInline(paragraph10, createInlineText1());
-    const paragraph11 = factory.node.createParagraphNode();
-    transform.node.appendInline(paragraph11, createInlineText1());
-    const paragraph12 = factory.node.createParagraphNode();
-    transform.node.appendInline(paragraph12, createInlineText2());
-    transform.node.appendInline(paragraph12, createInlineText3());
-    transform.node.appendInline(paragraph12, createInlineText4());
-    const paragraph13 = factory.node.createParagraphNode();
-    transform.node.appendInline(paragraph13, createInlineText1());
+    const paragraph1 = factory.item.createParagraph();
+    transform.item.appendInline(paragraph1, createInlineText1());
+    const paragraph2 = factory.item.createParagraph();
+    paragraph2.indent.increment(1);
+    transform.item.appendInline(paragraph2, createInlineText1());
+    const paragraph3 = factory.item.createParagraph();
+    paragraph3.indent.increment(2);
+    transform.item.appendInline(paragraph3, createInlineText2());
+    transform.item.appendInline(paragraph3, createInlineText3());
+    transform.item.appendInline(paragraph3, createInlineText4());
+    const paragraph4 = factory.item.createParagraph();
+    paragraph4.indent.increment(2);
+    transform.item.appendInline(paragraph4, createInlineText1());
+    const paragraph5 = factory.item.createParagraph();
+    transform.item.appendInline(paragraph5, createInlineText1());
+    const paragraph6 = factory.item.createParagraph();
+    transform.item.appendInline(paragraph6, createInlineText1());
+    const paragraph7 = factory.item.createParagraph();
+    paragraph7.indent.increment(1);
+    transform.item.appendInline(paragraph7, createInlineText1());
+    const paragraph8 = factory.item.createParagraph();
+    paragraph8.indent.increment(2);
+    transform.item.appendInline(paragraph8, createInlineText2());
+    transform.item.appendInline(paragraph8, createInlineText3());
+    transform.item.appendInline(paragraph8, createInlineText4());
+    const paragraph9 = factory.item.createParagraph();
+    transform.item.appendInline(paragraph9, createInlineText1());
+    const paragraph10 = factory.item.createParagraph();
+    transform.item.appendInline(paragraph10, createInlineText1());
+    const paragraph11 = factory.item.createParagraph();
+    paragraph11.indent.increment(1);
+    transform.item.appendInline(paragraph11, createInlineText1());
+    const paragraph12 = factory.item.createParagraph();
+    paragraph12.indent.increment(2);
+    transform.item.appendInline(paragraph12, createInlineText2());
+    transform.item.appendInline(paragraph12, createInlineText3());
+    transform.item.appendInline(paragraph12, createInlineText4());
+    const paragraph13 = factory.item.createParagraph();
+    paragraph13.indent.increment(1);
+    transform.item.appendInline(paragraph13, createInlineText1());
 
-    transform.node.append(paragraph2, paragraph3);
-    transform.node.append(paragraph2, paragraph4);
-    transform.node.append(paragraph1, paragraph2);
-    transform.node.append(document, paragraph1);
-    transform.node.append(document, paragraph5);
-    transform.node.append(paragraph7, paragraph8);
-    transform.node.append(paragraph6, paragraph7);
-    transform.node.append(document, paragraph6);
-    transform.node.append(document, paragraph9);
-    transform.node.append(paragraph11, paragraph12);
-    transform.node.append(paragraph10, paragraph11);
-    transform.node.append(paragraph10, paragraph13);
-    transform.node.append(document, paragraph10);
+    transform.item.append(paragraph1);
+    transform.item.append(paragraph2);
+    transform.item.append(paragraph3);
+    transform.item.append(paragraph4);
+    transform.item.append(paragraph5);
+    transform.item.append(paragraph6);
+    transform.item.append(paragraph7);
+    transform.item.append(paragraph8);
+    transform.item.append(paragraph9);
+    transform.item.append(paragraph10);
+    transform.item.append(paragraph11);
+    transform.item.append(paragraph12);
+    transform.item.append(paragraph13);
   });
-  memberDoc.merge(new CRDTDocument(user.id, userDoc.save()));
+  memberDoc.merge(userDoc.save(), user.id);
 
   userDoc.change((doc) => {
     const document = doc.document;
     const selection = doc.users[user.id];
-    const firstNode = document.nodes[0] as ParagraphNode;
+    const firstNode = document.items[0] as ParagraphItem;
 
     selection.anchor = firstNode.id;
     selection.focus = firstNode.id;
-    selection.range = factory.selection.createRange(firstNode.inline[0].id, firstNode.inline[0].text.length);
+    selection.range = factory.range.create(firstNode.inline[0].id, firstNode.inline[0].text.length);
   });
-  memberDoc.merge(new CRDTDocument(user.id, userDoc.save()));
+  memberDoc.merge(userDoc.save(), user.id);
 
   memberDoc.change((doc) => {
     const document = doc.document;
-    const selection = factory.selection.createSelection();
-    const firstNode = document.nodes[0] as ParagraphNode;
+    const selection = factory.selection.create();
+    const firstNode = document.items[0] as ParagraphItem;
 
     selection.anchor = firstNode.id;
     selection.focus = firstNode.id;
-    selection.range = factory.selection.createRange(firstNode.inline[0].id, firstNode.inline[0].text.length);
+    selection.range = factory.range.create(firstNode.inline[0].id, firstNode.inline[0].text.length);
 
     doc.users[member.id] = selection;
   });
-  userDoc.merge(new CRDTDocument(member.id, memberDoc.save()));
+  memberDoc.merge(userDoc.save(), user.id);
 
   return {
     user,
@@ -192,52 +200,53 @@ export function createSampleData2() {
     id: factory.uuid(),
   };
 
-  const userDoc = new CRDTDocument(user.id);
-  const memberDoc = new CRDTDocument(member.id, userDoc.save());
+  const userDoc = new BoardHandler(user.id);
+  const memberDoc = new BoardHandler(member.id, userDoc.save());
 
   userDoc.change((doc) => {
     const document = doc.document;
 
     const transform = transformation(document);
 
-    transform.node.remove(document.nodes[0]);
+    transform.item.remove(document.items[0]);
 
-    const heading1 = factory.node.createHeading1Node();
-    transform.node.appendInline(heading1, createInlineText1());
-    const paragraph1 = factory.node.createParagraphNode();
-    transform.node.appendInline(paragraph1, createInlineText1());
-    const heading2 = factory.node.createHeading1Node();
-    transform.node.appendInline(heading2, createInlineText1());
+    const heading1 = factory.item.createHeading1();
+    transform.item.appendInline(heading1, createInlineText1());
+    const paragraph1 = factory.item.createParagraph();
+    transform.item.appendInline(paragraph1, createInlineText1());
+    const heading2 = factory.item.createHeading1();
+    heading2.indent.increment(1);
+    transform.item.appendInline(heading2, createInlineText1());
 
-    transform.node.append(document, heading1);
-    transform.node.append(paragraph1, heading2);
-    transform.node.append(document, paragraph1);
+    transform.item.append(heading1);
+    transform.item.append(paragraph1);
+    transform.item.append(heading2);
   });
-  memberDoc.merge(new CRDTDocument(user.id, userDoc.save()));
+  memberDoc.merge(userDoc.save(), user.id);
 
   userDoc.change((doc) => {
     const document = doc.document;
     const selection = doc.users[user.id];
-    const firstNode = document.nodes[0] as Heading1Node;
+    const firstNode = document.items[0] as Heading1Item;
 
     selection.anchor = firstNode.id;
     selection.focus = firstNode.id;
-    selection.range = factory.selection.createRange(firstNode.inline[0].id, firstNode.inline[0].text.length);
+    selection.range = factory.range.create(firstNode.inline[0].id, firstNode.inline[0].text.length);
   });
-  memberDoc.merge(new CRDTDocument(user.id, userDoc.save()));
+  memberDoc.merge(userDoc.save(), user.id);
 
   memberDoc.change((doc) => {
     const document = doc.document;
-    const selection = factory.selection.createSelection();
-    const firstNode = document.nodes[0] as Heading1Node;
+    const selection = factory.selection.create();
+    const firstNode = document.items[0] as Heading1Item;
 
     selection.anchor = firstNode.id;
     selection.focus = firstNode.id;
-    selection.range = factory.selection.createRange(firstNode.inline[0].id, firstNode.inline[0].text.length);
+    selection.range = factory.range.create(firstNode.inline[0].id, firstNode.inline[0].text.length);
 
     doc.users[member.id] = selection;
   });
-  userDoc.merge(new CRDTDocument(member.id, memberDoc.save()));
+  memberDoc.merge(userDoc.save(), user.id);
 
   return {
     user,
