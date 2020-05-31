01b3b292932ae41488ecd2fd0171ecb81587a2a7
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-06-01 07:56:15 +0900

    Update index file for UI

--- UI/src/index.tsx
+++ UI/src/index.tsx
@@ -1,6 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import { BoardHandler, factory } from 'olot';
+
+import { Paper } from './components/Paper';
+import { Pencil } from './components/Pencil';
+
+const user = {
+  id: factory.uuid(),
+};
+
 window.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<div>OK</div>, window.document.querySelector('#root'));
+  const boardHandler = new BoardHandler(user.id);
+
+  ReactDOM.render(
+    <div>
+      <Pencil userId={user.id} boardHandler={boardHandler} />
+      <Paper userId={user.id} boardHandler={boardHandler} />
+    </div>,
+    window.document.querySelector('#root'),
+  );
 });
