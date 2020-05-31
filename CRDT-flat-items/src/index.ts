e1771bb8fd1ba0bdbb91d4168ff1cfe5fb07b8a2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-06-01 07:55:58 +0900

    Create index file for package

---
+++ CRDT-flat-items/src/index.ts
@@ -0,0 +1,7 @@
+export * from './BoardHandler';
+export * from './structures';
+export * from './actions';
+export * from './factory';
+export * from './transformation';
+export * from './traversal';
+export * from './usecases';
