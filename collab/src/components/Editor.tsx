faa49c8d3ad15daf32d472652f93b14bd27f63ad
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-06-13 23:05:36 +0900

    Update Editor

--- collab/src/components/Editor.tsx
+++ collab/src/components/Editor.tsx
@@ -1,48 +1,25 @@
-import * as Y from 'yjs';
-import { WebsocketProvider } from 'y-websocket';
-
 import * as React from 'react';
 
-type EditorProps = {};
+import { Item } from '../model';
 
-type EditorState = {
-  items: Y.Array<string>;
+type EditorProps = {
+  items: ReturnType<Item['toJSON']>[];
 };
 
-const doc = new Y.Doc();
+type EditorState = {};
 
 export class Editor extends React.Component<EditorProps, EditorState> {
   constructor(props: EditorProps) {
     super(props);
 
-    this.state = {
-      items: doc.getArray('items'),
-    };
-  }
-
-  public componentDidMount(): void {
-    const wsProvider = new WebsocketProvider('ws://localhost:5001', 'my-roomname', doc);
-    wsProvider.on('status', (event: any) => {
-      console.log(event.status);
-    });
-
-    doc.on('update', () => {
-      console.log('update');
-      this.setState({ items: doc.getArray('items') });
-    });
-
-    const items: Y.Array<string> = doc.getArray('items');
-    items.insert(0, [new Date().toString()]);
-    setInterval(() => {
-      items.insert(0, [new Date().toString()]);
-    }, 10000);
+    // noop
   }
 
   public render(): JSX.Element {
     return (
       <ul>
-        {this.state.items.map((item: string) => {
-          return <li key={item}>{item}</li>;
+        {this.props.items.map((item) => {
+          return <li key={item.id}>{item.inlines.map((inline) => inline.text.join())}</li>;
         })}
       </ul>
     );
