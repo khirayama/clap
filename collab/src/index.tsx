9e7af21340e198048d43c45343fd3a1a16c3c1cb
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-06-19 22:13:56 +0900

    Create sample data

--- collab/src/index.tsx
+++ collab/src/index.tsx
@@ -1,3 +1,4 @@
+import * as uuid from 'uuid';
 import * as Y from 'yjs';
 import { WebsocketProvider } from 'y-websocket';
 
@@ -11,13 +12,42 @@ wsProvider.on('status', (event: any) => {
   console.log(event.status);
 });
 
+type Item = Y.Map<{
+  id: string;
+  indent: number;
+  type: string;
+  text: Y.Text | null;
+}>;
+
+const factory = {
+  createItem: (): Item => {
+    const item: Item = new Y.Map(
+      Object.entries({
+        id: uuid.v4(),
+        indent: 0,
+        type: 'paragraph',
+        text: new Y.Text(),
+      }),
+    );
+    return item;
+  },
+};
+
 type EditorProps = {};
 
 type EditorState = {};
 
 class Editor extends React.Component<EditorProps, EditorState> {
   public render(): JSX.Element {
-    return <div>This is editor</div>;
+    const item = factory.createItem();
+    const inline: Y.Text = doc.getText('inline');
+    inline.insert(0, 'AAA', { bold: true });
+    inline.insert(1, 'BBB', { bold: false });
+    console.log(inline);
+    console.log(inline.toString());
+    console.log(inline.toDelta());
+
+    return <div>{inline.toString()}</div>;
   }
 }
 
